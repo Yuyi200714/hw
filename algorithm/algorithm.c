15676c2e446d8edaee1de016cb7b2e69661c662f
Yuyi200714 <1799334149@qq.com>
2024-10-15 12:31:15 +0000

    GreedyPlus

--- algorithm/algorithm.c
+++ algorithm/algorithm.c
@@ -74,6 +74,53 @@ int32_t Greedy(const InputParam *input, OutputParam *output)
     return RETURN_OK;
 }
 
+int32_t GreedyPlus(const InputParam *input, OutputParam *output)
+{
+    int32_t ret;
+    output->len = input->ioVec.len;
+    output->sequence = (uint32_t *)malloc(output->len * sizeof(uint32_t));
+    if (output->sequence == NULL) {
+        return RETURN_ERROR;
+    }
+
+    // 初始化已处理标记数组
+    int *processed = (int *)calloc(input->ioVec.len, sizeof(int));
+    if (processed == NULL) {
+        free(output->sequence);
+        return RETURN_ERROR;
+    }
+
+    HeadInfo currentHead = input->headInfo;
+    for(uint32_t i = 0; i < output->len;i++){
+        uint32_t minSeekTime = MAX_LPOS;
+        uint32_t minIndex = 0;
+
+        //寻找时间最短的IO请求
+        for(uint32_t j = 0; j < input->ioVec.len; j++){
+            if(processed[j] == 0){
+                HeadInfo temp;
+                temp.wrap = input->ioVec.ioArray[j].wrap;
+                temp.lpos = input->ioVec.ioArray[j].startLpos;
+                temp.status = HEAD_RW;
+                uint32_t seekTime = SeekTimeCalculate(&currentHead, &temp);
+                uint32_t IOS = abs(input->ioVec.ioArray[j].endLpos - input->ioVec.ioArray[j].startLpos);
+                double ans =  IOS*1.0/(IOS+seekTime)-abs(input->ioVec.ioArray[j].endLpos - currentHead.lpos)*1.0/IOS;
+                if(ans < minSeekTime){
+                    minSeekTime = ans;
+                    minIndex = j;
+                }
+            }
+        }
+        output->sequence[i] = input->ioVec.ioArray[minIndex].id;
+        currentHead.wrap = input->ioVec.ioArray[minIndex].wrap;
+        currentHead.lpos = input->ioVec.ioArray[minIndex].endLpos;
+        currentHead.status = HEAD_RW;
+        processed[minIndex] = 1;
+    }
+
+    return RETURN_OK;
+}
+
 int32_t FIFO(const InputParam *input, OutputParam *output){
     int32_t ret;
      /* 算法示例：先入先出算法 */
